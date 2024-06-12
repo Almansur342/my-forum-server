c3590fe51a2c2197d087d0cebff5bbfbb5bdbcf8
Almansur342 <abdullahalmansur2002@gmail.com>
2024-06-12 21:14:36 +0600

    delete post successfull from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -76,6 +76,13 @@ async function run() {
     console.log(email)
     const query = {hostEmail:email}
     const result = await postCollection.find(query).toArray()
+    res.send(result)
+  })
+
+  app.delete('/deletePost/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await postCollection.deleteOne(query)
     res.send(result)
   })
 
